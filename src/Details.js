7308e35662ac86c7c555600c02688a07dcca886d
Gus Escola <camagrock@gmail.com>
2019-06-05 18:30:43 +0200

    info for details page

--- src/Details.js
+++ src/Details.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./CSS/details.css";
 import styled from "styled-components";
 import Grid from "styled-components-grid";
+import Loader from "react-loader-spinner";
 export class Details extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-      isLoading: false,
+      data: { details: [], credits: [] },
+      isLoading: true,
       componentName: null
     };
 
@@ -16,19 +17,26 @@ export class Details extends React.Component {
 
   async componentDidMount() {
     try {
-      const details_query =
+      const credits_query =
         "https://api.themoviedb.org/3/movie/" +
         this.props.location.state.data.id +
         "/credits?api_key=" +
         process.env.REACT_APP_MOVIEDB_API_KEY +
         "&language=ES";
-
-      const response = await fetch(details_query);
+      const details_query =
+        "https://api.themoviedb.org/3/movie/" +
+        this.props.location.state.data.id +
+        "?api_key=" +
+        process.env.REACT_APP_MOVIEDB_API_KEY +
+        "&language=ES";
+      const details_response = await fetch(details_query);
+      const credits_response = await fetch(credits_query);
       this.setState({ isLoading: true });
-      const json = await response.json();
-      console.log(json);
+      const details_json = await details_response.json();
+      const credits_json = await credits_response.json();
+      console.log(credits_json);
       this.setState({
-        data: json.cast,
+        data: { credits: credits_json, details: details_json },
         isLoading: false,
         componentName: this.props.location.state.data.title
       });
@@ -37,54 +45,123 @@ export class Details extends React.Component {
       console.log("Error ==> ", err);
     }
   }
-
   render() {
     const backgroundImgUrl =
       "https://image.tmdb.org/t/p/w1280" +
       this.props.location.state.data.backdrop_path;
     const StylesDetail = styled.div`
-      
-      /* background: rgba(255,0,0,0); */
-      /* background-image: url(${backgroundImgUrl}); */
-      background-image: linear-gradient(to left, rgba(0,0,0, 0) 5%, rgba(13,15,17, 100)), url(${backgroundImgUrl});
-      /* background-size: "cover"; */
-      /* background-repeat:no-repeat; */
+      background-image: linear-gradient(
+          to left,
+          rgba(0, 0, 0, 0) 5%,
+          rgba(13, 15, 17, 100)
+        ),
+        url(${backgroundImgUrl});
       background-position: center center;
-      width: 100%; 
+      width: 100%;
       height: 100%;
-      /* background-image: url(${backgroundImgUrl}), linear-gradient(#eb01a5, #d13531); */
-      
     `;
 
-    const cast = this.state.data.map((data, index) => {
-      return (
-        <li>
-          <img
-            className="avatar"
-            src={"https://image.tmdb.org/t/p/w45/" + data.profile_path}
-            alt={"movie_" + index}
-          />
-          <span>
-            {data.name} --> {data.character}
-          </span>
-        </li>
-      );
-    });
+    //ALL CAST WITH PHOTO DETAIL
+    // const cast = this.state.data.cast.map((data, index) => {
+    //   return (
+    //     <li>
+    //       <img
+    //         className="avatar"
+    //         src={"https://image.tmdb.org/t/p/w45/" + data.profile_path}
+    //         alt={"movie_" + index}
+    //       />
+    //       <span>
+    //         {data.name} --> {data.character}
+    //       </span>
+    //     </li>
+    //   );
+    // });
 
-    return (
-      <React.Fragment>
-        <Grid halign="justify">
-          <Grid.Unit size={3 / 6} style={{ minHeight: "45em" }}>
-            <h3>Sinópsis</h3>
-            <p>{this.props.location.state.data.overview}</p>
-            <h3>Reparto</h3>
-            {/* <ul>{cast}</ul> */}
-          </Grid.Unit>
-          <Grid.Unit size={3 / 6} style={{ minHeight: "45em" }}>
-            <StylesDetail>{/* <ul>{cast}</ul> */}</StylesDetail>
-          </Grid.Unit>
-        </Grid>
-      </React.Fragment>
-    );
+    if (this.state.isLoading) {
+      return <span>Loading</span>;
+    }
+    if (this.state.isLoading == false) {
+      let release_date = this.state.data.details.release_date.split("-");
+      let duration = " " + this.state.data.details.runtime + " min";
+      //GENRES DETAIL
+      let genres_correct = [];
+      this.state.data.details.genres.map(data => {
+        genres_correct.push(data.name);
+      });
+      genres_correct = genres_correct.join(", ");
+      console.log("GENRES:", genres_correct);
+      return (
+        <React.Fragment>
+          <Grid halign="justify">
+            <Grid.Unit
+              size={3 / 6}
+              style={{ minHeight: "45em", padding: "2em" }}
+            >
+              <h3>{this.props.location.state.data.title}</h3>
+              <div className="title_bottom_info">
+                <span className="gray_text">{release_date[0]}</span>
+                <span className="gray_text">{duration}</span>
+              </div>
+
+              <p className="sinopsis">
+                {this.props.location.state.data.overview}
+              </p>
+              <Grid halign="left">
+                <Grid.Unit size={0.25}>
+                  <span className="gray_text">Director</span>
+                </Grid.Unit>
+                <Grid.Unit size={0.75}>
+                  <span>{this.state.data.credits.crew[0].name}</span>
+                </Grid.Unit>
+              </Grid>
+              <Grid halign="left">
+                <Grid.Unit size={0.25}>
+                  <span className="gray_text">Reparto</span>
+                </Grid.Unit>
+                <Grid.Unit size={0.75}>
+                  <span>
+                    {this.state.data.credits.cast[0].name},
+                    {" " + this.state.data.credits.cast[1].name},
+                    {" " + this.state.data.credits.cast[2].name}...
+                  </span>
+                </Grid.Unit>
+              </Grid>
+              <Grid halign="left">
+                <Grid.Unit size={0.25}>
+                  <span className="gray_text">Géneros</span>
+                </Grid.Unit>
+                <Grid.Unit size={0.75}>{genres_correct}</Grid.Unit>
+              </Grid>
+              <Grid halign="left">
+                <Grid.Unit size={0.25}>
+                  <span className="gray_text">Título Original</span>
+                </Grid.Unit>
+                <Grid.Unit size={0.75}>
+                  <span>{this.state.data.details.original_title}</span>
+                </Grid.Unit>
+              </Grid>
+
+              {/* <div className="sinopsis_bottom_info">
+                <div>
+                  <span className="gray_text">Director</span>
+                </div>
+                <div>
+                  <span className="gray_text">Reparto</span>
+                </div>
+                <div>
+                  <span className="gray_text">Géneros</span>
+                </div>
+              </div> */}
+
+              {/* <h3>Reparto</h3> */}
+              {/* <ul>{cast}</ul> */}
+            </Grid.Unit>
+            <Grid.Unit size={3 / 6} style={{ minHeight: "45em" }}>
+              <StylesDetail>{/* <ul>{cast}</ul> */}</StylesDetail>
+            </Grid.Unit>
+          </Grid>
+        </React.Fragment>
+      );
+    }
   }
 }
--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -7,26 +7,29 @@ import {
   FormControl,
   Button
 } from "react-bootstrap";
-
+import mainLogo from "../images/logo_adso_ok.png";
+import "../CSS/navigationBar.css";
+import { Link } from "react-router-dom";
 export const NavigationBar = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">
+  <Navbar bg="dark" expand="lg">
+    <Navbar.Brand href="/#home">
       {" "}
-      <span className="brand_name_bold">ADSO</span>
-      <span className="brand_name_regular">films</span>
+      <img alt="Adso Films" src={mainLogo} className="logo" />
+      {/* <span className="brand_name_bold">ADSO</span>
+      <span className="brand_name_regular">films</span> */}
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        <NavDropdown title="Catálogo" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Estrenos</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Series</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Películas</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">Documentales</NavDropdown.Item>
+          {/* <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
         </NavDropdown>
+        <Nav.Link href="#home">Contacto</Nav.Link>
       </Nav>
       <Form inline>
         <FormControl
@@ -34,7 +37,7 @@ export const NavigationBar = () => (
           placeholder="Introduce un título del catálogo"
           className="mr-sm-2"
         />
-        <Button variant="outline-success">Buscar</Button>
+        <i class="fas fa-search" />
       </Form>
     </Navbar.Collapse>
   </Navbar>
